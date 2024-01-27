21e198f2247d0f64daf85e4001aa1249e93ed781
Eric Li <eric.li.8736@gmail.com>
2024-01-27 17:32:03 -0600

    Add sign in button

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import { UserButton } from "@clerk/nextjs";
+import { SignInButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-6xl font-bold">Log in page</h1>
+      <SignInButton />
       <UserButton afterSignOutUrl="/"/>
     </main>
   );
