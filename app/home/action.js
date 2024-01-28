ecfb03cdef1dbcc039cab5a4762400b541f7b7aa
jeff-ooi <jeffooi@tamu.edu>
2024-01-28 11:12:26 -0600

    create new group working

--- app/home/action.js
+++ app/home/action.js
@@ -1,5 +1,6 @@
 "use server";
 import { revalidateTag } from "next/cache";
+import { redirect } from "next/navigation";
 
 export const revalidate = () => {
   revalidateTag("todos");
@@ -15,4 +16,22 @@ export const deleteTodo = async (id, personal) => {
     body: JSON.stringify(body),
   });
   revalidate();
+};
+
+
+export const createNewGroup = async (name, password) => {
+  // console.log(name, password)
+  const url = 'http://localhost:9000' //TODO: CHANGE
+  var response = await fetch(url+'/addGroup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({name, password})
+  })
+  var res = await response.json();
+  // console.log(res);
+  const id = res.updateSuccess;
+  
+  redirect(`/home/group/${id}`)
 };
