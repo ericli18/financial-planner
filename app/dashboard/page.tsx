2f5358e6750125343866290bf5caa2ec2cb15f2e
Aadit Shah <aadit2805@gmail.com>
2024-01-27 20:33:34 -0600

    Signout button

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,10 @@
-import { Todo, defaultColumns } from "./Columns"
-import { DataTable } from "./DataTable"
-import Form from "./EventForm"
-import { currentUser } from '@clerk/nextjs';
+import { Todo, defaultColumns } from "./Columns";
+import { DataTable } from "./DataTable";
+import Form from "./EventForm";
+import { SignInButton, SignOutButton, useAuth } from "@clerk/nextjs";
+import './dashboard.css'; // Make sure this path is correct for your CSS file
+import Link from 'next/link'; // Import Link from Next.js
+
 
 async function getData(): Promise<Todo[]> {
   return [
@@ -23,16 +26,22 @@ async function getData(): Promise<Todo[]> {
       dueDate: "2020-11-03",
       completed: false,
     }
-  ]
+  ];
 }
 
 export default async function Page() {
-  const data = await getData()
+  const data = await getData();
 
   return (
     <main className="container mx-auto py-10 min-h-screen flex flex-col gap-12 items-center">
       <Form />
       <DataTable columns={defaultColumns} data={data} />
+      <div className="signOutButtonContainer">
+        {}
+        <Link href="/" passHref>
+            <SignOutButton />
+        </Link>
+      </div>
     </main>
-  )
+  );
 }
