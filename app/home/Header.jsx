57f177649e94d14b4a59a5c97be70e3863c91bdd
Eric Li <eric.li.8736@gmail.com>
2024-01-28 10:11:57 -0600

    Add suspense ish to profile

--- app/home/Header.jsx
+++ app/home/Header.jsx
@@ -1,16 +1,27 @@
 import Link from "next/link";
-import { currentUser, UserButton, SignInButton } from "@clerk/nextjs";
+import {
+  currentUser,
+  UserButton,
+  SignInButton,
+  ClerkLoaded,
+  ClerkLoading,
+} from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./Dropdown";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./Dropdown";
 import { DropdownMenuItemIndicator, DropdownMenuSeparator } from "./Dropdown";
-import '../globals.css'; // Import the stylesheet
+import "../globals.css"; // Import the stylesheet
 import { Dialog, DialogHeader, DialogTitle } from "./Dialog";
 import { DialogContent, DialogDescription, DialogTrigger } from "./Dialog";
 import { Input } from "@/components/Input";
 import { Label } from "@/components/Label";
 import { Button } from "@/components/Button";
-import HeaderForm from "./HeaderForm"
-
+import HeaderForm from "./HeaderForm";
+import { Circle, CircleUserRound } from "lucide-react";
 
 const url = process.env.REACT_APP_URL || "http://localhost:9000";
 
@@ -19,78 +30,89 @@ export default async function Header() {
   if (!user) {
     redirect("/");
   }
-  var response = await fetch(url+'/getUserFromEmail', {
-    method: 'POST',
+  var response = await fetch(url + "/getUserFromEmail", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({email: 'jeffooi@tamu.edu'})
-  })
+    body: JSON.stringify({ email: "jeffooi@tamu.edu" }),
+  });
   var data = await response.json();
   const id = data.user.id;
-  
-  response = await fetch(url+'/getAllGroupsForUser', {
-    method: 'POST',
+
+  response = await fetch(url + "/getAllGroupsForUser", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({userId: id})
-  })
+    body: JSON.stringify({ userId: id }),
+  });
   data = await response.json();
   const groups = data.groups;
   return (
-    <nav className="min-w-full flex justify-between shadow-sm px-12 py-4">
-      <Link href ='/home'>Home</Link>
+    <nav className='min-w-full flex justify-between shadow-sm px-12 py-4'>
+      <Link href='/home'>Home</Link>
       <HeaderForm />
-      <div className="flex gap-8 justify-end">
+      <div className='flex gap-8 justify-end'>
         <DropdownMenu>
           <DropdownMenuTrigger>Groups</DropdownMenuTrigger>
           <DropdownMenuContent>
-            {
-              groups.map((group, i) => {
-                return (
-                  <Link href ={`/home/group/${group.id}`} key={i}>
-                    <DropdownMenuItem className = 'center-flex' key={i}>
-                      {group.name}
-                    </DropdownMenuItem>
-                  </Link>
-                )
-              })
-            }
+            {groups.map((group, i) => {
+              return (
+                <Link href={`/home/group/${group.id}`} key={i}>
+                  <DropdownMenuItem className='center-flex' key={i}>
+                    {group.name}
+                  </DropdownMenuItem>
+                </Link>
+              );
+            })}
             <DropdownMenuSeparator />
-            <DropdownMenuItem asChild>
+            <DropdownMenuItem className='center-flex' asChild>
               <Dialog>
-                <DialogTrigger className="center-flex">Create Group</DialogTrigger>
+                <DialogTrigger className='center-flex min-w-full max-w-full'>
+                  Create Group
+                </DialogTrigger>
                 <DialogContent>
                   <DialogHeader>
                     <DialogTitle>Create Group</DialogTitle>
                     <DialogDescription>Create a new Group</DialogDescription>
                   </DialogHeader>
-                    <Label>Name</Label>
-                      <Input id="name" />
-                    <Label>Password</Label>
-                      <Input id="password" />
-                    <Button type='submit'>Submit</Button>
+                  <Label>Name</Label>
+                  <Input id='name' />
+                  <Label>Password</Label>
+                  <Input id='password' />
+                  <Button type='submit'>Submit</Button>
                 </DialogContent>
               </Dialog>
             </DropdownMenuItem>
             <DropdownMenuSeparator />
-            <DropdownMenuItem asChild>
+            <DropdownMenuItem asChild className='center-flex'>
               <Dialog>
-                <DialogTrigger className="center-flex">Join Group</DialogTrigger>
+                <DialogTrigger className='center-flex min-w-full max-w-full'>
+                  Join Group
+                </DialogTrigger>
                 <DialogContent>
                   <DialogHeader>
                     <DialogTitle>Join Group</DialogTitle>
-                    <DialogDescription>Join an existing Group using a Password</DialogDescription>
+                    <DialogDescription>
+                      Join an existing Group using a Password
+                    </DialogDescription>
                   </DialogHeader>
-                    <Label>Password</Label>
-                      <Input id="password" />
+                  <Label>Password</Label>
+                  <Input id='password' />
                 </DialogContent>
               </Dialog>
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
-        <UserButton />
+        <div className="w-full center-flex">
+          <ClerkLoading>
+            <div className="h-4 w-4"></div>
+          </ClerkLoading>
+          <ClerkLoaded>
+            <UserButton />
+          </ClerkLoaded>
+        </div>
       </div>
     </nav>
   );
